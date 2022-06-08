ac6be71310d8aa81fd3efc34b2d54903ce155187
Naman <mymail.naman@gmail.com>
2022-06-08 05:42:56 +0530

    Designed the page

--- script.js
+++ script.js
@@ -16,3 +16,11 @@ function fetchRandomDogImage(){
 
 
 $('#dog-image-btn').click(fetchRandomDogImage);
+
+function generateCat() {
+    var image = document.createElement('img')
+    var div = document.getElementById('flex-cat-gen')
+    image.src = 'https://thecatapi.com/api/images/get?format=src&type=gif&size=small'
+    div.appendChild(image)
+
+}
