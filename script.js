ba1694ca5d9ad880713acb14eff859fe196cc0ba
Naman <mymail.naman@gmail.com>
2022-06-08 05:59:51 +0530

    Using Jquery

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 function fetchRandomDogImage(){
-    var xhrRequest = new XMLHttpRequest();
+    // var xhrRequest = new XMLHttpRequest();
 
-    xhrRequest.onload = function(){
-        var responseJSON = JSON.parse(xhrRequest.response);
-        var imageUrl = responseJSON.message;
+    // xhrRequest.onload = function(){
+    //     var responseJSON = JSON.parse(xhrRequest.response);
+    //     var imageUrl = responseJSON.message;
+    //     $('#dog-image').attr('src', imageUrl);
+    // };
+
+    // xhrRequest.open('get', 'https://dog.ceo/api/breeds/image/random', true);
+    // xhrRequest.send();
+
+    $.get('https://dog.ceo/api/breeds/image/random', function (data) {
+        var imageUrl = data.message;
         $('#dog-image').attr('src', imageUrl);
-    };
+    })
 
-    xhrRequest.open('get', 'https://dog.ceo/api/breeds/image/random', true);
-    xhrRequest.send();
 }
+
+
 
 $('#dog-image-btn').click(fetchRandomDogImage);
