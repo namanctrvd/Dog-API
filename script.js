563b06a80bda959f2ed0c0af2f443a870a29d477
Naman <mymail.naman@gmail.com>
2022-06-08 05:45:11 +0530

    Done

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ function fetchRandomDogImage(){
     var xhrRequest = new XMLHttpRequest();
 
     xhrRequest.onload = function(){
-        console.log(xhrRequest.response);
         var responseJSON = JSON.parse(xhrRequest.response);
         var imageUrl = responseJSON.message;
-        console.log(imageUrl)
         $('#dog-image').attr('src', imageUrl);
     };
 
@@ -13,14 +11,4 @@ function fetchRandomDogImage(){
     xhrRequest.send();
 }
 
-
-
 $('#dog-image-btn').click(fetchRandomDogImage);
-
-function generateCat() {
-    var image = document.createElement('img')
-    var div = document.getElementById('flex-cat-gen')
-    image.src = 'https://thecatapi.com/api/images/get?format=src&type=gif&size=small'
-    div.appendChild(image)
-
-}
