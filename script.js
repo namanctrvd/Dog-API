54ad3e9c1a818553a07f49402a7384c86594eea7
Naman <mymail.naman@gmail.com>
2022-06-08 05:30:51 +0530

    Getting the image from the API

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-function fetchrandomDogImage(){
-    var xhrRequest = new 
+function fetchRandomDogImage(){
+    var xhrRequest = new XMLHttpRequest();
+
+    xhrRequest.onload = function(){
+        console.log(xhrRequest.response);
+        var responseJSON = JSON.parse(xhrRequest.response);
+        var imageUrl = responseJSON.message;
+        console.log(imageUrl)
+        $('#dog-image').attr('src', imageUrl);
+    };
+
+    xhrRequest.open('get', 'https://dog.ceo/api/breeds/image/random', true);
+    xhrRequest.send();
 }
 
 
 
-
-
-$('#dog-image-btn').click(fechrandomDogImage);
+$('#dog-image-btn').click(fetchRandomDogImage);
